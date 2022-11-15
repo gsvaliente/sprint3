b0666064ba329997b96259a7f01e2ead52c0dc62
Gabriel Valiente <gsvalient@gmail.com>
2022-11-15 09:37:17 +0100

    created JS files for singleton and .gitignore

---
+++ Jugador.js
@@ -0,0 +1,5 @@
+class Jugador {
+  constructor(username) {
+    this.username = username;
+  }
+}
---
+++ juego.js
@@ -0,0 +1,5 @@
+//GAME LOGIC
+
+let selection = prompt("Higher or Lower");
+let ranNumber = Math.floor(Math.random() * 10) + 1;
+console.log(ranNumber);

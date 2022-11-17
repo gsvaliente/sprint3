d5a4f284017ac5ca7e77b2f8288c5be26fb9af4c
Gabriel Valiente <gsvalient@gmail.com>
2022-11-17 09:29:45 +0100

    first try callback hell

--- callback_hell.js
+++ callback_hell.js
@@ -1,30 +1,39 @@
-const {
-  readdir,
-  readFile,
-  writeFile
-} = require("fs");
-const {
-  join
-} = require("path");
+const { readdir, readFile, writeFile } = require("fs");
+const { join } = require("path");
 const inbox = join(__dirname, "inbox");
 const outbox = join(__dirname, "outbox");
+const util = require("util");
 
-const reverseText = str =>
-  str
-  .split("")
-  .reverse()
-  .join("");
+const readdirPromise = util.promisify(readdir);
+const readFilePromise = util.promisify(readFile);
+const writeFilePromise = util.promisify(writeFile);
+
+const reverseText = (str) => str.split("").reverse().join("");
 
 // Read and reverse contents of text files in a directory
 readdir(inbox, (error, files) => {
   if (error) return console.log("Error: Folder inaccessible");
-  files.forEach(file => {
+  files.forEach((file) => {
     readFile(join(inbox, file), "utf8", (error, data) => {
       if (error) return console.log("Error: File error");
-      writeFile(join(outbox, file), reverseText(data), error => {
+      writeFile(join(outbox, file), reverseText(data), (error) => {
         if (error) return console.log("Error: File could not be saved!");
         console.log(`${file} was successfully saved in the outbox!`);
       });
     });
   });
 });
+
+const start = async () => {
+  try {
+    const first = await readdirPromise(inbox);
+    const second = await first.forEach(file);
+    const third = await readFilePromise(join((inbox, second), "utf-8"));
+    await writeFilePromise(join(outbox, third), reverseText(data));
+    console.log(`${third} was successfully saved in the outbox!`);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
--- juego.js
+++ juego.js
@@ -1,5 +1,1 @@
 //GAME LOGIC
-
-let selection = prompt("Higher or Lower");
-let ranNumber = Math.floor(Math.random() * 10) + 1;
-console.log(ranNumber);

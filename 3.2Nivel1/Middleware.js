dcc6937144dcb03ac206672d162ae4d9ed8fc54b
Gabriel Valiente <gsvalient@gmail.com>
2022-12-07 16:38:38 +0100

    created file for decorator assignment

--- 3.2Nivel1/Middleware.js
+++ 3.2Nivel1/Middleware.js
@@ -1,3 +1,13 @@
-class Middleware {}
+class Middleware {
+  square(a) {
+    return a ** 2;
+  }
+  cube(a) {
+    return a ** 3;
+  }
+  division(a, b) {
+    return a / b;
+  }
+}
 
-module.exports(Middleware);
+module.exports = Middleware;
---
+++ 3.2Nivel2/Product.js
@@ -0,0 +1,7 @@
+class Product {
+  constructor(name, price, currency) {
+    this.name = name;
+    this.price = price;
+    this.currency = currency;
+  }
+}

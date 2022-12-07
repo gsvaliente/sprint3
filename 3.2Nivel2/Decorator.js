fd6c2302fc0c2e73867b177ca9df4eb4f954c20d
Gabriel Valiente <gsvalient@gmail.com>
2022-12-07 22:20:16 +0100

    created decorator and start test

--- 3.2Nivel2/Decorator.js
+++ 3.2Nivel2/Decorator.js
@@ -1,1 +1,24 @@
-const conversioRates = require();
+const conversionRates = require("./currencyRates.json");
+
+class Decorator {
+  constructor(product) {
+    this.product = product;
+  }
+
+  get getProduct() {
+    return this.product;
+  }
+
+  get getCurrency() {
+    return this.getProduct.currency;
+  }
+
+  convert(currency) {
+    const re = new RegExp(this.getCurrency + "_" + currency);
+    const conversion = this.getProduct.price * Object.entries(conversionRates).find((k) => re.test(k))[1];
+    console.log(`${this.getCurrency} converted to ${currency}`);
+    console.log(conversion);
+  }
+}
+
+module.exports = Decorator;
---
+++ 3.2Nivel2/app.js
@@ -0,0 +1,8 @@
+const Decorator = require("./Decorator");
+const Product = require("./Product");
+
+// CREATING PRODUCTS
+const playStation = new Product("PlayStation 5", 499, "USD");
+const xbox = new Product("Xbox Series X", 399, "GPB");
+
+// TESTING DECORATOR

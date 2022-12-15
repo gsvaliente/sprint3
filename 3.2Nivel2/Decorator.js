236ebc78651bd63fcbbe0137e7dc59e65f8b295e
Gabriel Valiente <gsvalient@gmail.com>
2022-12-15 10:54:42 +0100

    maybe fixing decorator

--- 3.2Nivel2/Decorator.js
+++ 3.2Nivel2/Decorator.js
@@ -1,31 +1,46 @@
-const conversionRates = require("./currencyRates.json");
+const conversionRates = require('./currencyRates.json');
+const Product = require('./Product');
 
-class Decorator {
-  constructor(product) {
-    this.product = product;
-  }
+// class Decorator {
+//   constructor(product) {
+//     this.product = product;
+//   }
 
-  get getProduct() {
-    return this.product;
-  }
+//   get getProduct() {
+//     return this.product;
+//   }
 
-  get getCurrency() {
-    return this.getProduct.currency;
-  }
+//   get getCurrency() {
+//     return this.getProduct.currency;
+//   }
 
-  get getPrice() {
-    return this.getProduct.price;
-  }
+//   get getPrice() {
+//     return this.getProduct.price;
+//   }
 
-  convert(currency) {
-    if (currency !== "EUR") {
-      throw "Calculator only converts to EUR";
-    }
-    const keyRate = `${this.getCurrency}_${currency}`;
-    const conversion = (this.getPrice * conversionRates[keyRate]).toFixed(2);
-    console.log(`${this.getCurrency} converted to ${currency}`);
-    console.log(`${this.getProduct.name} is ${conversion} EUROS\n`);
-  }
-}
+//   convert(currency) {
+//     if (currency !== "EUR") {
+//       throw "Calculator only converts to EUR";
+//     }
+//     const keyRate = `${this.getCurrency}_${currency}`;
+//     const conversion = (this.getPrice * conversionRates[keyRate]).toFixed(2);
+//     console.log(`${this.getCurrency} converted to ${currency}`);
+//     console.log(`${this.getProduct.name} is ${conversion} EUROS\n`);
+//   }
+// }
 
-module.exports = Decorator;
+const playStation = new Product('PlayStation 5', 499, 'USD');
+
+const decorator = (item) => {
+  item.convert = (currency) => {
+    const keyRate = `${item.currency}_${currency}`;
+    const conversion = (item.price * conversionRates[keyRate]).toFixed(2);
+    console.log(`${item.name} converted to ${currency}`);
+    console.log(`${item.name} is ${conversion} EUROS\n`);
+  };
+};
+
+decorator(playStation);
+playStation.convert('EUR');
+
+module.exports = decorator;
--- 3.2Nivel2/app.js
+++ 3.2Nivel2/app.js
@@ -1,11 +1,11 @@
-const Decorator = require("./Decorator");
-const Product = require("./Product");
+const Decorator = require('./Decorator');
+const Product = require('./Product');
 
 // CREATING INSTANCES OF THE PRODUCT CLASS
-const playStation = new Product("PlayStation 5", 499, "USD");
-const xbox = new Product("Xbox Series X", 399, "GBP");
-const appleWatch = new Product("Apple Watch Series 8", 96637, "JPY");
-const fitBit = new Product("FITBIT Versa 4", 200, "CHF");
+const playStation = new Product('PlayStation 5', 499, 'USD');
+const xbox = new Product('Xbox Series X', 399, 'GBP');
+const appleWatch = new Product('Apple Watch Series 8', 96637, 'JPY');
+const fitBit = new Product('FITBIT Versa 4', 200, 'CHF');
 
 // DECORATING PRODUCT TO HAVE A NEW METHOD OF CONVERTING
 const ps5 = new Decorator(playStation);
@@ -18,8 +18,8 @@ const products = [ps5, xboxX, watch, fitbit];
 
 // CONVERTING THE PRICE TO EUROS
 products.forEach((product) => {
-  product.convert("EUR");
+  product.convert('EUR');
 });
 
 // IF TRYING TO CONVERT TO A CURRENCY THAT IS NOT EUR
-ps5.convert("GBP"); // THROWS AN ERROR
+ps5.convert('GBP'); // THROWS AN ERROR
